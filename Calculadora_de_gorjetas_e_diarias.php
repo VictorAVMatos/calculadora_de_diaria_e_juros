d5cbba181cb1634030fc882a722234e7d5abc978
Victor Alexandre Vieira de Matos <112444309+VictorAVMatos@users.noreply.github.com>
2023-05-11 16:26:43 -0300

    Add files via upload

---
+++ Calculadora_de_gorjetas_e_diarias.php
@@ -0,0 +1,16 @@
+<?php
+// entregador de delivery ganha 80 reais por dia porém ainda tem
+// gorjetas de alguns clientes
+
+$valorDia = 80;
+$gorjetas = 40;
+$diaria = $valorDia + $gorjetas;
+
+echo"Segunda-feira: $diaria\n";
+
+$gorjetas = 15;
+$diaria = $valorDia + $gorjetas;
+
+echo "Terça-feira: $diaria";
+
+?>

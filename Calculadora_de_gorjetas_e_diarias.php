cc29be74e94e129e693fb070b93e02f8b2ee1511
Victor Alexandre Vieira de Matos <112444309+VictorAVMatos@users.noreply.github.com>
2023-05-11 16:38:15 -0300

    Update Calculadora_de_gorjetas_e_diarias.php

--- Calculadora_de_gorjetas_e_diarias.php
+++ Calculadora_de_gorjetas_e_diarias.php
@@ -1,6 +1,7 @@
 <?php
-// entregador de delivery ganha 80 reais por dia porém ainda tem
-// gorjetas de alguns clientes
+// Um determinado entregador de delivery ganha R$80,00 por dia, porém ainda tem
+// gorjetas de alguns clientes a receber durante as entregas
+// criei esta "calculadora" para auxilia-lo a descobrir o valor total recebido ao final dos dias trabalhados
 
 $valorDia = 80;
 $gorjetas = 40;
